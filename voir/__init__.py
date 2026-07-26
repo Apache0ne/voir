5e965d0bb808060e38bd9c1fbc885c59cc938592
Koston Speakman <xcoolcat502@gmail.com>
2026-07-26 19:33:21 -0400

    Export standalone sampler APIs

--- voir/__init__.py
+++ voir/__init__.py
@@ -1,18 +1,27 @@
-"""VOIR: frozen image reservoirs with trainable dense readouts."""
+"""VOIR: frozen image reservoirs with standalone Beta + DPM++ SDE sampling."""
 
+from .mage_sampler import MageDPMppSDEEditSampler, MageSamplerConfig
 from .readout import AlbedoReadout
 from .reservoir import CaptureConfig, MageEditReservoir, ToyImageReservoir
-from .schedule import beta_flow_sigmas, rescale_sigmas
+from .sampling import BrownianTreeNoiseSampler, SamplerTrace, sample_dpmpp_sde_gpu
+from .schedule import beta_flow_sigmas, beta_scheduler_from_model_sigmas, flow_model_sigmas, rescale_sigmas
 from .state import ReservoirState
 
 __all__ = [
     "AlbedoReadout",
+    "BrownianTreeNoiseSampler",
     "CaptureConfig",
+    "MageDPMppSDEEditSampler",
     "MageEditReservoir",
+    "MageSamplerConfig",
     "ReservoirState",
+    "SamplerTrace",
     "ToyImageReservoir",
     "beta_flow_sigmas",
+    "beta_scheduler_from_model_sigmas",
+    "flow_model_sigmas",
     "rescale_sigmas",
+    "sample_dpmpp_sde_gpu",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
