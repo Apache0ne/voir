4f74deed155d6fdc8369e1f7cdd3ff2c733ca638
Koston Speakman <xcoolcat502@gmail.com>
2026-07-26 21:39:04 -0400

    Export albedo v2 feature and metric APIs

--- voir/__init__.py
+++ voir/__init__.py
@@ -1,6 +1,8 @@
 """VOIR: frozen image reservoirs with standalone Beta + DPM++ SDE sampling."""
 
+from .albedo_features import AUXILIARY_ALBEDO_CHANNELS, fixed_albedo_features
 from .mage_sampler import MageDPMppSDEEditSampler, MageSamplerConfig
+from .metrics import albedo_metrics, global_ssim, local_ssim
 from .readout import AlbedoReadout
 from .reservoir import CaptureConfig, MageEditReservoir, ToyImageReservoir
 from .sampling import BrownianTreeNoiseSampler, SamplerTrace, sample_dpmpp_sde_gpu
@@ -8,6 +10,7 @@ from .schedule import beta_flow_sigmas, beta_scheduler_from_model_sigmas, flow_m
 from .state import ReservoirState
 
 __all__ = [
+    "AUXILIARY_ALBEDO_CHANNELS",
     "AlbedoReadout",
     "BrownianTreeNoiseSampler",
     "CaptureConfig",
@@ -17,11 +20,15 @@ __all__ = [
     "ReservoirState",
     "SamplerTrace",
     "ToyImageReservoir",
+    "albedo_metrics",
     "beta_flow_sigmas",
     "beta_scheduler_from_model_sigmas",
+    "fixed_albedo_features",
     "flow_model_sigmas",
+    "global_ssim",
+    "local_ssim",
     "rescale_sigmas",
     "sample_dpmpp_sde_gpu",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
